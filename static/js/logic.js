4deb4ff17599e4526086b9ce5388b254717e52c5
faithlierheimer <flierheimer@gmail.com>
2020-10-13 19:26:30 -0600

    a;lsdjkf

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,4 +1,15 @@
 //try to read in nfl data
-d3.csv("nfl.csv", (d) => {
-    console.log(d)
-})
+// d3.csv("nfl.csv").then(function(nfl) {
+//     console.log(nfl);
+//     // pray to the goddess
+//     });
+
+d3.csv("hours-of-tv-watched.csv").then(function(tvData) {
+
+    // Print the tvData
+    console.log(tvData);
+  
+    // Cast the hours value to a number for each piece of tvData
+    // tvData.forEach(function(data) {
+    //   data.hours = +data.hours;
+    });
