140336d05c883ed61fb9870c2130b165b9d75f27
faithlierheimer <flierheimer@gmail.com>
2020-10-13 19:47:19 -0600

    i am DEAD

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,15 +1,6 @@
 //try to read in nfl data
-// d3.csv("nfl.csv").then(function(nfl) {
-//     console.log(nfl);
-//     // pray to the goddess
-//     });
 
-d3.csv("hours-of-tv-watched.csv").then(function(tvData) {
-
+d3.csv("nfl.csv").then(function(data) {
     // Print the tvData
-    console.log(tvData);
-  
-    // Cast the hours value to a number for each piece of tvData
-    // tvData.forEach(function(data) {
-    //   data.hours = +data.hours;
+    console.log(data);
     });
