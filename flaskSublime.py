a0887c007c1eca56ea8435b011bcd503ae5f2c29
Nate <nathanieldiamond97@gmail.com>
2020-10-17 13:28:38 -0600

    why wont flask work i need a flask

---
+++ flaskSublime.py
@@ -0,0 +1,33 @@
+#Import flask
+import flask
+
+from flask_cors import CORS, cross_origin
+#Import dependencies for queries to include in endpoints
+############################
+from matplotlib import style
+style.use('seaborn')
+import matplotlib.pyplot as plt   
+import numpy as np                          
+import pandas as pd
+import datetime as dt
+import pprint as pp
+from datetime import timedelta
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+##########################
+#Set up connection to sqlite database
+## Create engine
+engine = create_engine('sqlite:///sports.db')
+## Declare a base
+Base = automap_base()
+## Use a base class to reflect NFL db tables
+Base.prepare(engine, reflect = True)
+## Double check connection brought in right tables 
+Tables = Base.classes.keys()
+#Save nfl table ref to its own variable
+mlb = Base.classes.mlb
+
+
+print(Tables)
