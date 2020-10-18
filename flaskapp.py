468e9a626dae32f28232ba6a6baf9d8246e63ffd
faithlierheimer <flierheimer@gmail.com>
2020-10-18 15:39:26 -0600

    I FIGURED OUT MULTIPLE DATA POINTS IN FLASK! BOW TO ME!

--- flaskapp.py
+++ flaskapp.py
@@ -21,6 +21,7 @@ from sqlalchemy import create_engine, func
 ##########################
 #Set up connection to sqlite database
 ## Create engine
+###THIS WILL ONLY WORK IF SUBSEQUENT DATA HAS GONE THRU THE "ADD A PRIMARY KEY" PROCESS THAT KEVIN WALKED US THROUGH.
 engine = create_engine('sqlite:///sports.db')
 
 ## Declare a base
@@ -33,40 +34,54 @@ Base.prepare(engine, reflect = True)
 Tables = Base.classes.keys()
 
 #Save nfl table ref to its own variable
+####USE THIS FORMAT TO SAVE TABLE REFS TO OWN VARIABLES FOR NHL, NBA, AND TICKET DATA. 
 nfl = Base.classes.nfl
 mlb3 = Base.classes.mlb3
 
 #Create a session to manage transactions to sqlite db
 session = Session(engine)
 
-##Get nfl data in so it can be jsonified?
+##Get nfl data in so it can be jsonified
 nfl_att = session.query(nfl.team, nfl.total_attendance, nfl.lat, nfl.long)
+##these column names below are custom---i made them slightly different than what is in the
+## actual df in jupyter notebook for ease of referencing in the logic.js. 
 nfl_att_df = pd.DataFrame(nfl_att, columns=['team', 'attendance', 'lat', 'long'])
 nfl_dict = nfl_att_df.to_dict('records')
 
-##Get mlb data 
+##Get mlb data  in via session.query so the dictionary can eventually be jsonified. 
 mlb_att = session.query(mlb3.Team_Names, mlb3.Home_attendance, mlb3.Per_Game, mlb3.Lat, mlb3.Long)
+## See above notes in line 47 re: column names 
 mlb_att_df = pd.DataFrame(mlb_att, columns = ['team', 'attendance', 'per_game', 'lat', 'long'])
 mlb_dict = mlb_att_df.to_dict('records')
-# print(nfl_dict)
+
+##NBA DATA TO DICTIONARY USING RECORDS METHOD TO EVENTUALLY BE JSONIFIED## 
+
+##NHL DATA TO DICTIONARY USING RECORDS METHOD TO EVENTUALLY BE JSONIFIED## 
+
+##TICKET DATA TO DICTIONARY USING RECORDS METHOD TO EVENTUALLY BE JSONIFIED##
 
 app = Flask(__name__)
 CORS(app)
 @app.route("/")
 def home():
+###lol can someone else fill in the available routes & maybe make them clickable?
+##look for sqlalchemy hw, i'm pretty sure there's an example there
     print("Server received request for homepage.")
     return """Available routes: 
             FILL IN AVAILABLE ROUTES LATER"""
 
-@app.route("/api/v1.0/nfl")
-def nfl_attendance():
+@app.route("/api/v1.0/sports_attendance")
+def nfl_mlb_attendance():
     print("Server received request for NFL map page")
-    nfl_json = jsonify(nfl_dict)
-    return nfl_json
-@app.route("/api/v1.0/mlb")
-def mlb_attendance():
-    print("Its baseball")
-    mlb_json = jsonify(mlb_dict)
-    return mlb_json
+    ## In order to return multiple JSONs in one API endpoint, we have to 
+    ## put each dictionary of data into a bigger dictionary like below, and then 
+    ##the json will be labeled appropriately and we can grab it how we figured out
+    ## in class the other day. 
+    sport = {
+        "nfl": nfl_dict,
+        "mlb": mlb_dict
+    }
+    sports = jsonify(sport)
+    return sports
 if __name__ == "__main__":
     app.run(debug=True)
