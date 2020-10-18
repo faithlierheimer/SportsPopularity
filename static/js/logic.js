2c080d1f64382345dda1ba4668a61858367fa367
faithlierheimer <flierheimer@gmail.com>
2020-10-18 16:51:57 -0600

    put in places for NBA and NHL data

--- static/js/logic.js
+++ static/js/logic.js
@@ -12,6 +12,10 @@ d3.json("http://127.0.0.1:5000/api/v1.0/sports_attendance").then(function(sports
     //initialize MLB layer 
     mlb_data = []
 
+    //INITIALIZE NHL LAYER HERE
+    nhl_data = []
+    //INITIALIZE NBA LAYER HERE
+    nba_data = []
     //Make color scale for NFL attendance
     for(var i = 0; i<sportsdata.nfl.length; i++){
         var color = "";
@@ -51,6 +55,9 @@ d3.json("http://127.0.0.1:5000/api/v1.0/sports_attendance").then(function(sports
             mlb_color = "black";
         };
 
+    //INSERT COLOR SCALE FOR NHL LAYER HERE--BUT NAME THE COLOR NHL_COLOR or something INSTEAD OF COLOR. 
+
+    //INSERT COLOR SCALE FOR NBA LAYER HERE-BUT NAME THE COLOR NBA_COLOR or something INSTEAD OF COLOR. 
 
         //now put in a circle w/different size depending on attendance
         nfl_data.push(L.circle([sportsdata.nfl[i].lat, sportsdata.nfl[i].long], {
@@ -69,6 +76,18 @@ d3.json("http://127.0.0.1:5000/api/v1.0/sports_attendance").then(function(sports
         }) 
         .bindPopup(`<h3> ${sportsdata.mlb[i].team} </h3><hr> <h4> Attendance: ${sportsdata.mlb[i].attendance} </h4>`));
         
+        //INSERT CIRCLES ABOUT ATTENDANCE FOR NHL DATA--TAKE OUT .ADDTO(MYMAP) PIECE. 
+        nhl_data.push(
+        //circles data on the inside of this. 
+
+
+        );
+
+        //INSERT CIRCLES ABOUT ATTENDANCE FOR NBA DATA--TAKE OUT .ADDTO(MYMAP) PIECE. 
+        nba_data.push(
+            //circles data on the inside of this. 
+
+        );
     };
 
     //make nfl data layer 
@@ -76,6 +95,13 @@ d3.json("http://127.0.0.1:5000/api/v1.0/sports_attendance").then(function(sports
 
     //make mlb data layer
     var mlb = L.layerGroup(mlb_data);
+
+    //make nhl data layer 
+    var nhl = L.layerGroup(nhl_data);
+
+    //make nba data layer 
+    var nba = L.layerGroup(nba_data);
+
     //streetmap and darkmap layer 
     var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
@@ -102,7 +128,9 @@ d3.json("http://127.0.0.1:5000/api/v1.0/sports_attendance").then(function(sports
      // Create overlay object to hold our overlay layer
     var overlayMaps = {
         NFL: nfl,
-        MLB: mlb
+        MLB: mlb,
+        NHL: nhl,
+        NBA: nba
     };
 
     // Create our map, giving it the streetmap and earthquakes layers to display on load
